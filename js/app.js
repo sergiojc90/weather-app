5663da061a62e584589f2db63ebf11b263ccfa13
sergiojc90 <sergiojc90@gmail.com>
2021-06-16 16:33:12 -0500

    Adding comments

--- js/app.js
+++ js/app.js
@@ -1,17 +1,23 @@
-    (function searchCity() {
+    (function weatherApp() {
+
+        // Defining main constants
         const btn = document.getElementById("search-btn");
         const cityName = document.getElementById("cityName");
         const countries = document.getElementById("countries");
 
+        // Main events on page load
         window.addEventListener("onload",getData("london","uk","metric"));
         btn.addEventListener("click",validateData);
 
+        // Function to validate that the user has entered a city and a country.
+        // The app requires a country due to the duplicated city names in diferent countries.
         function validateData(){
             if(cityName.value === "" || countries.value === "") return console.log("Choose a city and a country");
 
             getData(cityName.value, countries.value,"metric");
         }
 
+        // Function to display the weather data in the DOM
         function displayDom(weatherObject) {
             const icon = document.getElementById("icon");
             const temperature = document.getElementById("temperature");
@@ -20,6 +26,7 @@
             const humidity = document.getElementById("humidity");
             const wind = document.getElementById("wind");
 
+            // The URL of the icon is provided in https://openweathermap.org/weather-conditions
             icon.src = `http://openweathermap.org/img/wn/${weatherObject.weather[0].icon}@2x.png`;
             city.textContent = weatherObject.name;
             country.textContent = weatherObject.sys.country;
@@ -28,19 +35,24 @@
             wind.textContent = "Wind: " + weatherObject.wind.speed + "km/h";
         }
 
+        // Asyncronous function to fetch API data, it uses the name of a city, the country and the units (metric or imperial)
         async function getData(cityName,country,units){
             const weatherKey = "c61e36e895bb91ce2746c1c42d97aa6f";
             const response = await fetch(`http://api.openweathermap.org/data/2.5/find?q=${cityName},${country}&units=${units}&appid=${weatherKey}`,{mode:"cors"});
             
+            //Basic request status validation, if the status is not 200 OK, there is going to be a alert for the user and the function is going to stop execution
             if(response.status !== 200) return alert("Ooops, something went wrong, try again")
             
+            // If the response is 200, we are going to fetch the weather data
             const weatherData = await response.json();
 
+            // Validate that there is at least one city with the provided name in the country
             if (weatherData.count === 0) return alert("There is no such city in that country :p");
 
+            //Getting the firs city object of the array of cities provided by the API
             const weatherObject = weatherData.list[0];
-            displayDom(weatherObject);
 
-            return weatherData;
+            //  Display the weather object in the DOM
+            return displayDom(weatherObject);
         }
     })();
