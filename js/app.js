d8430da3c50f6e88d396867251f74cd09ea13853
sergiojc90 <sergiojc90@gmail.com>
2021-06-16 11:43:23 -0500

    fetching, returning and validating the json response

--- js/app.js
+++ js/app.js
@@ -4,15 +4,23 @@
         const country = document.getElementById("countries");
 
         btn.addEventListener("click",(e) => {
-            e.preventDefault;
-            console.log(getData(cityName.value, country.value,"metric")); 
+            if(cityName.value === "" || country.value === "") return console.log("Choose a city and a country");
+
+            const data = getData(cityName.value, country.value,"metric");
+
+            
         });
 
         async function getData(cityName,country,units){
             const weatherKey = "c61e36e895bb91ce2746c1c42d97aa6f";
-            const weatherData = await fetch(`http://api.openweathermap.org/data/2.5/find?q=${cityName},${country}&units=${units}&appid=${weatherKey}`,{mode:"cors"});
+            const response = await fetch(`http://api.openweathermap.org/data/2.5/find?q=${cityName},${country}&units=${units}&appid=${weatherKey}`,{mode:"cors"});
             
-            return weatherData.json();
+            if(response.status !== 200) return console.log("Ooops, something went wrong, try again")
+            
+            const weatherData = await response.json();
+
+            if (weatherData.count === 0) return console.log("Nothing to do here");
+            return weatherData;
         }
     })();
     
