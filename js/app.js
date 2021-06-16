a91d219fb81030398756f1131568cf0c7dbf0033
sergiojc90 <sergiojc90@gmail.com>
2021-06-16 14:02:26 -0500

    Creating constants for dom manipulation

--- js/app.js
+++ js/app.js
@@ -1,25 +1,34 @@
     (function searchCity() {
         const btn = document.getElementById("search-btn");
         const cityName = document.getElementById("cityName");
-        const country = document.getElementById("countries");
+        const countries = document.getElementById("countries");
 
-        btn.addEventListener("click",(e) => {
-            if(cityName.value === "" || country.value === "") return console.log("Choose a city and a country");
+        const icon = document.getElementById("icon");
+        const temperature = document.getElementById("temperature");
+        const city = document.getElementById("city");
+        const country = document.getElementById("country");
+        const humidity = document.getElementById("humidity");
+        const wind = document.getElementById("wind");
 
-            const data = getData(cityName.value, country.value,"metric");
+        btn.addEventListener("click",render);
 
-            
-        });
+        function render(){
+            if(cityName.value === "" || countries.value === "") return console.log("Choose a city and a country");
+
+            getData(cityName.value, countries.value,"metric");
+        }
 
         async function getData(cityName,country,units){
             const weatherKey = "c61e36e895bb91ce2746c1c42d97aa6f";
             const response = await fetch(`http://api.openweathermap.org/data/2.5/find?q=${cityName},${country}&units=${units}&appid=${weatherKey}`,{mode:"cors"});
             
-            if(response.status !== 200) return console.log("Ooops, something went wrong, try again")
+            if(response.status !== 200) return Error("Ooops, something went wrong, try again")
             
             const weatherData = await response.json();
 
-            if (weatherData.count === 0) return console.log("Nothing to do here");
+            if (weatherData.count === 0) return Error("There is no such city in that countr :p");
+
+            console.table(weatherData.list[0].main)
             return weatherData;
         }
     })();
