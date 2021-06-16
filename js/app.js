ff185107e86cfbd17790addb188491de390c76cf
sergiojc90 <sergiojc90@gmail.com>
2021-06-16 15:49:51 -0500

    Renaming render funciton to validateData, adding event to window

--- js/app.js
+++ js/app.js
@@ -3,33 +3,44 @@
         const cityName = document.getElementById("cityName");
         const countries = document.getElementById("countries");
 
-        const icon = document.getElementById("icon");
-        const temperature = document.getElementById("temperature");
-        const city = document.getElementById("city");
-        const country = document.getElementById("country");
-        const humidity = document.getElementById("humidity");
-        const wind = document.getElementById("wind");
+        window.addEventListener("onload",getData("london","uk","metric"));
+        btn.addEventListener("click",validateData);
 
-        btn.addEventListener("click",render);
-
-        function render(){
+        function validateData(){
             if(cityName.value === "" || countries.value === "") return console.log("Choose a city and a country");
 
             getData(cityName.value, countries.value,"metric");
+        }
+
+        function displayDom(weatherObject) {
+            const icon = document.getElementById("icon");
+            const temperature = document.getElementById("temperature");
+            const city = document.getElementById("city");
+            const country = document.getElementById("country");
+            const humidity = document.getElementById("humidity");
+            const wind = document.getElementById("wind");
+
+            icon.src = `http://openweathermap.org/img/wn/${weatherObject.weather[0].icon}@2x.png`;
+            city.textContent = weatherObject.name;
+            country.textContent = weatherObject.sys.country;
+            temperature.textContent = "Temperature: " + weatherObject.main.temp + "°C";
+            humidity.textContent = "Humidity: " + weatherObject.main.humidity + "%";
+            wind.textContent = "Wind: " + weatherObject.wind.speed + "km/h";
         }
 
         async function getData(cityName,country,units){
             const weatherKey = "c61e36e895bb91ce2746c1c42d97aa6f";
             const response = await fetch(`http://api.openweathermap.org/data/2.5/find?q=${cityName},${country}&units=${units}&appid=${weatherKey}`,{mode:"cors"});
             
-            if(response.status !== 200) return Error("Ooops, something went wrong, try again")
+            if(response.status !== 200) return alert("Ooops, something went wrong, try again")
             
             const weatherData = await response.json();
 
-            if (weatherData.count === 0) return Error("There is no such city in that countr :p");
+            if (weatherData.count === 0) return alert("There is no such city in that country :p");
 
-            console.table(weatherData.list[0].main)
+            const weatherObject = weatherData.list[0];
+            displayDom(weatherObject);
+
             return weatherData;
         }
     })();
-    
